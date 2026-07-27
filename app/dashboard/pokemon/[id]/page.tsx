2864a411653d4bb17feaa3d7a348709e34ffe5ef
Karla Espinosa <karlae2703@gmail.com>
2026-07-26 18:25:13 -0600

    fix width

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -61,7 +61,7 @@ export default async function PokemonPage({ params }: Props) {
 
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
-      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
+      <div className="relative flex flex-col items-center rounded-[20px] w-175 mx-auto bg-white bg-clip-border  shadow-lg  p-3">
         <div className="mt-2 mb-8 w-full">
           <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
             #{pokemon.id} {pokemon.name}
